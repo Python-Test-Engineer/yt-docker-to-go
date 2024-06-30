d75918daf3448cb67c4afd9c3111956a215eb750
Craig West <iwswordpress@gmail.com>
2024-06-30 09:15:31 +0100

    notes for django

--- django/README.md
+++ django/README.md
@@ -1,17 +1,6 @@
 # Docker dev
 
-
-## Based on Very Academy
-
-https://www.youtube.com/watch?v=aMqs_y6dZw4&list=PLOLrQ9Pn6cazCfL7v4CdaykNoWMQymM_C&index=2
-
-
-## Changes
-
-- requirements.txt uptdate to django-5.0
-- Added templates
-
-
+[YT Video]()
 ## Set up
 
 Create venv and install...
@@ -19,3 +8,13 @@ Create venv and install...
 docker compose up -d --build
 
 http://127.0.0.1:8000/ 
+
+`docker exec -it django_container bash` to enter docker container
+
+Then you can do usual Django stuff...
+
+root@c46509fc6029:/django# python manage.py createsuperuser
+
+## Development
+
+Add code as usual in the app.
--- django/core/views.py
+++ django/core/views.py
@@ -7,7 +7,11 @@ def index(request):
 
     now = datetime.now()  # current date and time
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    return HttpResponse(f"<h2>Hello, world! <h2><h2>The time is {date_time}<h2>")
+    output = f"<div style='width: 600px; margin: 40px auto'>"
+    output += f"<h2 style='color:green'>Hello, world! The time is {date_time}</h2>"
+    output += f"<p style='font-size: 20px'>Qui irure ea dolore magna sint fugiat sint excepteur adipisicing officia fugiat amet. Exercitation aute labore consectetur aliquip. Eu dolor do nulla culpa id voluptate laborum non. Adipisicing amet ullamco ad nostrud eiusmod laboris ad dolore aliqua velit occaecat enim non mollit.</p>"
+    output += "</div>"
+    return HttpResponse(output)
 
 
 def test(request):
