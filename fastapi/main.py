d2ba6114e176b811ee81c2535f0ec99c51750b58
Craig West <iwswordpress@gmail.com>
2024-07-03 09:48:49 +0100

    add endpoints to fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,7 @@
 from os import environ as env
 from fastapi import FastAPI
+import requests
+
 
 app = FastAPI()
 
@@ -9,4 +11,20 @@ def index():
     DB_URL = env["DB_URL"]
     print(DB_URL)
     output = f"Hot reloading works! 👏 DB_URL = {DB_URL} 🚀"
-    return {"details": output}
+    return {"details": output, "success": True, "error": None}
+
+
+@app.get("/users")
+def get_users():
+
+    # Making a GET request
+    response = requests.get("https://api.github.com/users/python-test-engineer")
+
+    # check status code for response received
+    # success code - 200
+    output = response.json()
+    if response.status_code != 200:
+        output = {"details": output, "success": False, "error": "API Error"}
+    else:
+        output = {"details": output["login"], "success": True, "error": None}
+    return output
