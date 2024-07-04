e2c6a143fb2acc23781ed8d8f8c6789621867da9
Craig West <iwswordpress@gmail.com>
2024-07-04 09:51:00 +0100

    edit typo filename

--- django/core/views.py
+++ django/core/views.py
@@ -6,9 +6,9 @@ from datetime import datetime
 def index(request):
 
     now = datetime.now()  # current date and time
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    output = f"<div style='width: 600px; margin: 40px auto'>"
-    output += f"<h2 style='color:green'>Hello, world! The time is {date_time}</h2>"
+    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+    output = f"<div style='width: 600px; margin: 40px auto;font-family: sans-serif'>"
+    output += f"<h2 style='color:#676767'>Hello, world! The time is {date_time}</h2>"
     output += f"<p style='font-size: 20px'>Qui irure ea dolore magna sint fugiat sint excepteur adipisicing officia fugiat amet. Exercitation aute labore consectetur aliquip. Eu dolor do nulla culpa id voluptate laborum non. Adipisicing amet ullamco ad nostrud eiusmod laboris ad dolore aliqua velit occaecat enim non mollit.</p>"
     output += "</div>"
     return HttpResponse(output)
